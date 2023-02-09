3ec05059354d5e51f21b0876f4211391923ec7fa
Olusegun Mayungbe <o.mayungbe@oluadepefarms.com>
2023-02-09 22:40:38 +0100

    create table with four elments

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+--Create SQL Table "users" with attributes - id, email, name, & country
+
+CREATE TABLE if NOT EXISTS users (
+	id INT NOT NULL PRIMARY KEY,
+	email VARCHAR (255) UNIQUE,
+	name VARCHAR (255)
+	country ENUM ('US', 'CO' 'TN') NOT NULL DEFAULT 'US'
+);
