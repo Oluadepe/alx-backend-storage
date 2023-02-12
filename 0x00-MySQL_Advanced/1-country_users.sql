88d05c14488d3f60acc99755f0834d08bdffbbb6
Olusegun Mayungbe <o.mayungbe@oluadepefarms.com>
2023-02-12 23:03:01 +0100

    error identified and fixed

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,8 @@
 --Create SQL Table "users" with attributes - id, email, name, & country
 
-CREATE TABLE if NOT EXISTS users (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	email VARCHAR(255) NOT NULL UNIQUE,
-	name VARCHAR(255),
-	country ENUM('US', 'CO' 'TN') NOT NULL DEFAULT 'US'
-);
+CREATE TABLE IF NOT EXISTS `users` (  
+  `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `email` VARCHAR(255) NOT NULL UNIQUE,
+  `name` VARCHAR(255),
+  `country` ENUM('US', 'CO', 'TN') NOT NULL
+)
